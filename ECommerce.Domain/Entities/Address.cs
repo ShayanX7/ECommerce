0ec7ca838fe365e95102ea1006c1d1236303eaeb
shayan shirozhan <shayanshirozhan.dev@gmail.com>
2026-09-19 05:07:45 -0700

    feat: add address domain entity

---
+++ ECommerce.Domain/Entities/Address.cs
@@ -0,0 +1,16 @@
+﻿using ECommerce.Domain.Common;
+
+namespace ECommerce.Domain.Entities;
+
+public class Address : AuditableEntity
+{
+    public Guid UserId { get; private set; }
+    public string Title { get; private set; } = null!;
+    public string RecipientName { get; private set; } = null!;
+    public string PhoneNumber { get; private set; } = null!;
+    public string Province { get; private set; } = null!;
+    public string City { get; private set; } = null!;
+    public string PostalCode { get; private set; } = null!;
+    public string AddressLine { get; private set; } = null!;
+    public bool IsDefault { get; private set; }
+}
