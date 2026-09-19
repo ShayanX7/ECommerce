b262088f84f841bcc1c0824760f1e0b3b9ce20ef
shayan shirozhan <shayanshirozhan.dev@gmail.com>
2026-09-19 04:54:37 -0700

    chore: configure openapi

--- ECommerce.API/Program.cs
+++ ECommerce.API/Program.cs
@@ -6,11 +6,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddOpenApi();
+
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+if (app.Environment.IsDevelopment())
+    app.MapOpenApi();
 
 app.UseHttpsRedirection();
 
